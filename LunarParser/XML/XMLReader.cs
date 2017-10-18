bce6efc31b4a1d3cf6616b96b84209ba6c1dc7d2
Sergio Flores <relfos@gmail.com>
2017-10-18 21:59:31 +0100

    Rewrite of XML parser, now faster and with support for comments

--- LunarParser/XML/XMLReader.cs
+++ LunarParser/XML/XMLReader.cs
@@ -1,148 +1,299 @@
 ﻿using System;
-using System.Globalization;
+using System.Text;
 
 namespace LunarParser.XML
 {
-
     public class XMLReader
     {
-        private const char TAG_START = '<';
-        private const char TAG_END = '>';
-        private const char QUOTE = '"';
-        private const char SLASH = '/';
-        private const char EQUALS = '=';
-        private static string BEGIN_QUOTE = "" + EQUALS + QUOTE;
+        private enum State
+        {
+            Next,
+            Name,
+            Prolog,
+            Comment,
+            AttributeName,
+            AttributeQuote,
+            AttributeValue,
+            NextAttribute,
+            Content,
+        }
 
-        public static DataNode ReadFromString(string xml)
+
+        public static DataNode ReadFromString(string contents)
         {
             int index = 0;
-            int lastIndex = 0;
-            int prevIndex = 0;
-            DataNode rootNode = null;
-            DataNode currentNode = null;
-
-            bool openTag = false;
-
-            xml = xml.Replace(" \n", "");
-            xml = xml.Replace("\n", "");
-
-            while (true)
-            {
-                index = xml.IndexOf(TAG_START, lastIndex);
-
-                if (index < 0 || index >= xml.Length)
-                    break;
-
-                index++;
-
-                lastIndex = xml.IndexOf(TAG_END, index);
-                if (lastIndex < 0 || lastIndex >= xml.Length)
-                    break;
-
-
-                int tagLength = lastIndex - index;
-                string xmlTag = xml.Substring(index, tagLength);
-
-                if (xmlTag[0] == '?')
-                {
-                    continue;
-                }
-
-                if (xmlTag[0] == SLASH)
-                {
-                    currentNode.Value = xml.Substring(prevIndex + 1, (index - prevIndex) - 2);
-                    currentNode = currentNode.Parent;
-                    continue;
-                }
-
-                prevIndex = lastIndex;
-                openTag = true;
-
-                if (xmlTag[tagLength - 1] == SLASH)
-                {
-                    xmlTag = xmlTag.Substring(0, tagLength - 1);
-                    openTag = false;
-                }
-
-
-                DataNode node = ParseTag(xmlTag);
-
-                if (currentNode != null)
-                {
-                    currentNode.AddNode(node);
-                }
-
-                if (openTag || currentNode == null)
-                {
-                    currentNode = node;
-                }
-
-
-                if (rootNode == null)
-                {
-                    rootNode = node;
-                }
-            }
-
-            return rootNode;
+            var root = ReadNode(contents, ref index);
+            return root;
         }
 
-
-        private static DataNode ParseTag(string xmlTag)
+        private static string GetPos(string contents, int index)
         {
-            int nameEnd = xmlTag.IndexOf(' ', 0);
-            if (nameEnd < 0)
-            {
-                return DataNode.CreateObject(xmlTag);
-            }
-
-            string tagName = xmlTag.Substring(0, nameEnd);
-            var node = DataNode.CreateObject(tagName);
-
-            string attrString = xmlTag.Substring(nameEnd, xmlTag.Length - nameEnd);
-            ParseAttributes(attrString, node);
-
-            return node;
+            return "offset " + index;
         }
 
-        private static void ParseAttributes(string xmlTag, DataNode node)
+        private static DataNode ReadNode(string contents, ref int index)
         {
-            int index = 0;
-            int attrNameIndex = 0;
-            int lastIndex = 0;
+            DataNode result = null;
 
-            while (true)
+            var state = State.Next;
+            char c;
+
+            StringBuilder name_content = new StringBuilder();
+            StringBuilder value_content = new StringBuilder();
+
+            int rewind_index = index;
+
+            do
             {
-                index = xmlTag.IndexOf(BEGIN_QUOTE, lastIndex);
-                if (index < 0 || index > xmlTag.Length)
-                    break;
+                bool isWhiteSpace;
+                bool next = false;
+                bool inside = state == State.Content || state == State.Name || state == State.AttributeName || state == State.AttributeValue;
 
-                attrNameIndex++;
-                while (xmlTag[attrNameIndex] <= ' ')
+                do
                 {
-                    attrNameIndex++;
-                }
-                string attrName = xmlTag.Substring(attrNameIndex, index - attrNameIndex);
+                    if (index >= contents.Length)
+                    {
+                        throw new Exception($"XML parsing exception, unexpected end of data");
+                    }
 
-                index += 2;
+                    c = contents[index];
+                    isWhiteSpace = Char.IsWhiteSpace(c);
 
-                lastIndex = xmlTag.IndexOf(QUOTE, index);
-                if (lastIndex < 0 || lastIndex > xmlTag.Length)
+                    if (!isWhiteSpace)
+                    {
+                        rewind_index = index;
+                    }
+
+                    index++;
+
+
+                    next = isWhiteSpace && !inside;
+                } while (next);
+
+                switch (state)
                 {
-                    break;
+                    case State.Next:
+                        {
+                            switch (c)
+                            {
+                                case '<':
+                                    {                                        
+                                        state = State.Name;
+                                        name_content.Length = 0;
+                                        break;
+                                    }
+
+                                default:
+                                    {
+                                        throw new Exception($"XML parsingexception at {GetPos(contents, index)}, unexpected character");
+                                    }
+                            }
+                            break;
+                        }
+
+                    case State.Name:
+                        {
+                            switch (c)
+                            {
+                                case '?':
+                                    {
+                                        if (contents[index - 2] == '<')
+                                        {
+                                            state = State.Prolog;
+                                        }
+                                        else
+                                        {
+                                            name_content.Append(c);
+                                        }
+                                        break;
+                                    }
+
+                                case '!':
+                                    {
+                                        if (index< contents.Length-3 && contents[index - 2] == '<' && contents[index] == '-' && contents[index+1] == '-')
+                                        {
+                                            state = State.Comment;
+                                        }
+                                        else
+                                        {
+                                            name_content.Append(c);
+                                        }
+                                        break;
+                                    }
+
+                                case '/':
+                                    {
+                                        break;
+                                    }
+
+                                case '>':
+                                    {
+                                        result = DataNode.CreateObject(name_content.ToString());
+
+                                        if (contents[index-2] == '/')
+                                        {
+                                            return result;
+                                        }
+
+                                        state = State.Content;
+                                        break;
+                                    }
+
+                                case ' ':
+                                    {
+                                        result = DataNode.CreateObject(name_content.ToString());
+                                        name_content.Length = 0;
+                                        state = State.AttributeName;
+                                        break;
+                                    }
+
+                                default:
+                                    {
+                                        name_content.Append(c);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+
+                    case State.AttributeName:
+                        {
+                            switch (c)
+                            {
+                                case '=':
+                                    {
+                                        state = State.AttributeQuote;
+                                        break;
+                                    }
+
+                                default:
+                                    {
+                                        name_content.Append(c);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+
+                    case State.AttributeQuote:
+                        {
+                            if (c == '"')
+                            {
+                                state = State.AttributeValue;
+                                value_content.Length = 0;
+                            }
+                            else
+                            {
+                                throw new Exception($"XML parsingexception at {GetPos(contents, index)}, unexpected character");
+                            }
+                            break;
+                        }
+
+                    case State.AttributeValue:
+                        {
+                            switch (c)
+                            {
+                                case '"':
+                                    {
+                                        result.AddField(name_content.ToString(), value_content.ToString());
+                                        state = State.NextAttribute;
+                                        break;
+                                    }
+
+                                default:
+                                    {
+                                        value_content.Append(c);
+                                        break;
+                                    }
+                            }
+
+                            break;
+                        }
+
+                    case State.NextAttribute:
+                        {
+                            switch (c)
+                            {
+                                case '/':
+                                    {
+                                        break;
+                                    }
+
+                                case '>':
+                                    {
+                                        if (contents[index-2] == '/')
+                                        {
+                                            return result;
+                                        }
+
+                                        state = State.Content;
+
+                                        break;
+                                    }
+                            }
+
+                            break;
+                        }
+
+                    case State.Prolog:
+                        {
+                            if (c == '>')
+                            {
+                                state = State.Next;
+                            }
+                            break;
+                        }
+
+                    case State.Comment:
+                        {
+                            switch (c)
+                            {
+                                case '>':
+                                    {
+                                        if (contents[index - 2] == '-' && contents[index - 3] == '-')
+                                        {
+                                            state = State.Next;
+                                        }
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+
+                    case State.Content:
+                        {
+                            switch (c)
+                            {
+                                case '<':
+                                    {
+                                        if (index<contents.Length && contents[index] == '/')
+                                        {
+                                            result.Value = value_content.ToString();
+                                            return result;
+                                        }
+                                        else
+                                        {
+                                            index--;
+                                            var child = ReadNode(contents, ref index);
+                                            result.AddNode(child);
+                                        }
+                                        break;
+                                    }
+
+                                default:
+                                    {
+                                        value_content.Append(c);
+                                        break;
+                                    }
+                            }
+                            break;
+                        }
+
+
+
                 }
 
-                int tagLength = lastIndex - index;
-                string attrValue = xmlTag.Substring(index, tagLength);
-
-                node.AddField(attrName, attrValue);
-
-                attrNameIndex = lastIndex;
-                //Debug.Log("loaded node " + attrName);
-            }
-
+            } while (true);
         }
-
     }
-
 }
--- Tests/ParserTests.cs
+++ Tests/ParserTests.cs
@@ -23,6 +23,59 @@ namespace LunarParserTests
         }
 
         [Test]
+        public void TestXMLReaderFull()
+        {
+            var root = XMLReader.ReadFromString("<?xml version=\"1.0\" encoding=\"utf-8\"?><!-- In this collection, we will keep each title \"as is\" --><videos><video><title>The Distinguished Gentleman</title><director>Jonathan Lynn</director><length>112 Minutes</length><format>DVD</format><rating>R</rating></video><video><title>Her Alibi</title><director>Bruce Beresford</director><length>94 Mins</length><format>DVD</format><rating>PG-13</rating> </video></videos>");
+            Assert.NotNull(root);
+            Assert.IsTrue("videos".Equals(root.Name));
+            Assert.IsTrue(root.ChildCount.Equals(2));
+
+            var content = root.GetNode("video");
+            Assert.NotNull(content);
+            Assert.IsTrue(content.ChildCount.Equals(5));
+        }
+
+        [Test]
+        public void TestXMLComments()
+        {
+            var root = XMLReader.ReadFromString("<message><!--This is a comment, will be ignored--><content>Hello world!</content></message>");
+            Assert.NotNull(root);
+            Assert.IsTrue("message".Equals(root.Name));
+
+            var content = root.GetString("content");
+            Assert.IsFalse(string.IsNullOrEmpty(content));
+
+            Assert.IsTrue(content.Equals("Hello world!"));
+        }
+
+        [Test]
+        public void TestXMLProlog()
+        {
+            var root = XMLReader.ReadFromString("<!--This is a comment, will be ignored--><message><content>Hello world!</content></message>");
+            Assert.NotNull(root);
+            Assert.IsTrue("message".Equals(root.Name));
+            Assert.IsTrue(root.ChildCount.Equals(1));
+
+            var content = root.GetString("content");
+            Assert.IsFalse(string.IsNullOrEmpty(content));
+
+            Assert.IsTrue(content.Equals("Hello world!"));
+        }
+
+        [Test]
+        public void TestXMLAttributes()
+        {
+            var root = XMLReader.ReadFromString("<message content=\"Hello world!\"/>");
+            Assert.NotNull(root);
+            Assert.IsTrue("message".Equals(root.Name));
+
+            var content = root.GetString("content");
+            Assert.IsFalse(string.IsNullOrEmpty(content));
+
+            Assert.IsTrue(content.Equals("Hello world!"));
+        }
+
+        [Test]
         public void TestJSONReader()
         {
             var root = JSONReader.ReadFromString("{\"message\": { \"content\": \"Hello world!\"} }");
